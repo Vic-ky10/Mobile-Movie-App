2b702dd5ffc9aa028992a53bda023815817e10dd
vigneshwaran <vigneshvenketasan@gmail.com>
2026-03-18 11:35:35 +0530

    working with Routing

---
+++ app/movie/[id].tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>  Details</Text>
+    </View>
+  )
+}
+
+export default MovieDetails
+
+const styles = StyleSheet.create({
+    
+})
